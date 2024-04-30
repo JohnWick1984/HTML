beab0c2464be05d737d2f1c2daa91740c4c7b60e
Евгений Шумаков <daredevil1984@list.ru>
2024-04-30 16:24:31 +0500

    Добавьте файлы проекта.

---
+++ HTML/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using HtmlAgilityPack; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите URL страницы:");
+        string url = Console.ReadLine();
+
+        
+        WebClient client = new WebClient();
+        string html = client.DownloadString(url);
+
+       
+        HtmlDocument doc = new HtmlDocument();
+        doc.LoadHtml(html);
+
+      
+        string pageTitle = doc.DocumentNode.SelectSingleNode("//title").InnerText;
+        Console.WriteLine("Заголовок страницы: " + pageTitle);
+
+       
+        int tagCount = doc.DocumentNode.SelectNodes("//*").Count;
+        Console.WriteLine("Количество тегов на странице (без учета закрывающих тегов): " + tagCount);
+
+       
+        client.Dispose();
+    }
+}
